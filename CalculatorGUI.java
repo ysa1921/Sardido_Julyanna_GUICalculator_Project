e5923e542e677f63cc63b4a9c11a039c4c4d5f1c
Sardido, Julyanna Shania BSCPE <sardidoyanna@gmail.com>
2024-12-23 00:31:01 +0800

    Update CalculatorGUI.java

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -17,8 +17,8 @@ public class CalculatorGUI extends javax.swing.JFrame {
     double Result;
     String Op;
     
-    
-    public CalculatorGUI() {
+    {
+    }public CalculatorGUI() {
         initComponents();
     }
 
